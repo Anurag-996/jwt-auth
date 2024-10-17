a7ea1de1e8e7909997cff559d47ea1da466514f4
Anurag-996 <anuragvishwakarma546@gmail.com>
2024-10-17 18:18:15 +0530

    jwtfilter logs removed

--- src/main/java/com/vid/VideoCall/Jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/vid/VideoCall/Jwt/JwtAuthenticationFilter.java
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -19,7 +18,6 @@ import com.vid.VideoCall.CustomExceptions.TokenBlacklistedException;
 import java.io.IOException;
 
 @Component
-@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final HandlerExceptionResolver handlerExceptionResolver;
@@ -43,7 +41,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            log.error("I am in jwtFilter");
             filterChain.doFilter(request, response);
             return;
         }
